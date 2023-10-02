80f9d79eb40c271a69ad977c1040b42fd62dc2e9
Karolis Rusenas <karolis@rusenas.dev>
2023-10-02 20:08:27 +0100

    cfg

--- docker/slurmdbd/docker-entrypoint.sh
+++ docker/slurmdbd/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
-sudo service slurmctld start
+# sudo service slurmctld start
 
 slurmdbd -Dvvv
 # tail -f /dev/null
