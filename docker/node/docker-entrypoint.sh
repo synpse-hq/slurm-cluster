b2e6aad7230906f97fc97cede65538d970afaebb
Karolis Rusenas <karolis@rusenas.dev>
2023-11-04 17:34:48 +0000

    multinode

--- docker/node/docker-entrypoint.sh
+++ docker/node/docker-entrypoint.sh
@@ -2,8 +2,16 @@
 
 # sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm/slurm.conf
 
-
 sudo service munge start
-sudo slurmd -N $(hostname)
 
-tail -f /dev/null
+# Use the environment variables
+CONTROLLER_HOST=${CONTROLLER_HOST}
+CONTROLLER_PORT=${CONTROLLER_PORT}
+
+# Using synpse device name
+HOSTNAME=${SYNPSE_DEVICE_NAME}
+
+sudo echo "${CONTROLLER_HOST} slurmmaster" >> /etc/hosts
+
+# Start slurmd in debug/foreground mode
+exec sudo slurmd -Dvvv -N ${HOSTNAME} --conf-server ${CONTROLLER_HOST} ${CONTROLLER_PORT}
